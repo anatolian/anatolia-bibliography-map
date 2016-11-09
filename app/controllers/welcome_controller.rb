5f6559b007c6b8d041eb27147bfecfa96f66c6c2
LydLyd <yujieli@seas.upenn.edu>
2016-11-08 19:08:00 -0500

    front page formatted

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,9 +6,11 @@ class WelcomeController < ApplicationController
       puts "all"
     elsif params[:q] != nil
       if params[:q].size == 0
-        puts "Empty string"
-        redirect_to root_url
-        puts root_url
+        # puts "Empty string"
+        # redirect_to root_url
+        # puts root_url
+        flash[:success] = 'You left the search box empty. You will now be directed to a listing of all publications...'
+        puts "all"
       else
         render "search/search", :locals => {:keyword => params[:q]}
         puts params[:q]
