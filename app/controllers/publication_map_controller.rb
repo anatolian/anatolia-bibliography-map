e623df00ce85f1db75ab95d7cb18e1956602ee9b
humaneu <human0110@gmail.com>
2016-12-05 19:52:39 -0500

    update query on map

--- app/controllers/publication_map_controller.rb
+++ app/controllers/publication_map_controller.rb
@@ -3,36 +3,38 @@ class PublicationMapController < ApplicationController
   end
 	
   def search_by_point
-  	lat = params[:lat]
-  	lng = params[:lng]
+  	lat = params[:lat].to_f
+  	lng = params[:lng].to_f
 
   	loc_id = Array.new
-  	Location.all.each do |location|
-      coordinates = location.coordinates
-
-      values = coordinates.split(";")
-      smallestDis = Float::MAX
-      values.each do |value|
-        coordinate = value.split(",")
-        latitude = coordinate[0]
-        longitude = coordinate[1]
-
-        distance = distance([lat.to_f, lng.to_f], [latitude.to_f, longitude.to_f])
-        if distance < smallestDis
-          smallestDis = distance
-        end
+  	Location.all.each do |location| #iterate locations
+      if isWithinSearchRadius(location.coordinates, lat, lng)
+        loc_id << location.id
       end
-
-  		if  smallestDis < 100
-			 loc_id << location.id
-  		end
-  	end
+    end
 	
 	  @result = Article.where("location_id IN (?)", loc_id).joins(:publication)
   	render "publication_map/search_result", :locals => {:res => @result}
-
   end
 
+  def isWithinSearchRadius(coordinates, lat, lng)
+    polygons = coordinates.split("|")
+
+    polygons.each do |polygon| #iterate polygons
+      points = polygon.split(";")
+      points.each do |point| #iterate points
+        coordinate = point.split(",")
+        latitude = coordinate[0].to_f
+        longitude = coordinate[1].to_f
+
+        distance = distance([lat, lng], [latitude, longitude])
+        if distance < 100
+          return true
+        end
+      end
+    end
+    return false
+  end
 
   def distance loc1, loc2
   	rad_per_deg = Math::PI/180  # PI / 180
@@ -58,42 +60,44 @@ class PublicationMapController < ApplicationController
   	lat2 = params[:lat2].to_f
   	lng2 = params[:lng2].to_f
 
-    puts "Point 1"
-    puts lat1
-    puts lng1
-    puts lat2
-    puts lng2
-
   	lower_lat = [lat1, lat2].min
     higher_lat = [lat1, lat2].max
     lower_lng = [lng1, lng2].min
   	higher_lng = [lng1, lng2].max
 
-  	#puts "lower lat: #{lower_lat}, higher lat: #{higher_lat}"
-	  #puts "lower lng: #{lower_lng}, higher lng: #{higher_lng}"
+    puts "lower lat: #{lower_lat}, higher lat: #{higher_lat}"
+	  puts "lower lng: #{lower_lng}, higher lng: #{higher_lng}"
 
   	loc_id = Array.new
   	Location.all.each do |location|
-      coordinates = location.coordinates
-
-      values = coordinates.split(";")
-      values.each do |value|
-        coordinate = value.split(",")
-        latitude = coordinate[0].to_f
-        longitude = coordinate[1].to_f
-
-        if  latitude >= lower_lat && latitude <= higher_lat && longitude >= lower_lng && longitude <= higher_lng
-          loc_id << location.id
-          break
-        end
-
+      if isWithinSearchRectangle(location.coordinates, lower_lat, higher_lat, lower_lng, higher_lng)
+        loc_id << location.id
       end
-
   	end
 
     @result = Article.where("location_id IN (?)", loc_id).joins(:publication)
   	render "publication_map/search_result", :locals => {:res => @result}
+  end
 
+
+  def isWithinSearchRectangle(coordinates, lower_lat, higher_lat, lower_lng, higher_lng)
+    puts coordinates
+    polygons = coordinates.split("|")
+
+    polygons.each do |polygon| #iterate polygons
+      points = polygon.split(";")
+      points.each do |point| #iterate points
+        coordinate = point.split(",")
+        latitude = coordinate[0].to_f
+        longitude = coordinate[1].to_f
+
+        if  latitude >= lower_lat && latitude <= higher_lat && longitude >= lower_lng && longitude <= higher_lng
+          return true
+        end
+      end
+    end
+
+    return false
   end
 
 end
