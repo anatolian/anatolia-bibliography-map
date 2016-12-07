a1adb54907c53bf6dec747509b91323b1ad90eee
LydLyd <yujieli@seas.upenn.edu>
2016-12-07 00:08:10 -0500

    rails controller minitests for welcome_c + publication_map_c

--- test/controllers/publication_map_controller_test.rb
+++ test/controllers/publication_map_controller_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class PublicationMapControllerTest < ActionController::TestCase
-  test "should get search_by_map" do
-    get :search_by_map
+  
+  test "should get search_by_point" do
+    get :search_by_point
+    assert_response :success
+  end
+
+  test "should get search_by_rectangle" do
+    get :search_by_rectangle
     assert_response :success
   end
 
